f8f6d25c59687045923320391d5475880ed60692
JJdoubleA <jaytdot2k@gmail.com>
2021-04-07 14:08:42 -0400

    login3

--- WebApplication/Areas/Identity/Data/User.cs
+++ WebApplication/Areas/Identity/Data/User.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApplication.Areas.Identity.Data
 {
     // Add profile data for application users by adding properties to the User class
     public class User : IdentityUser
     {
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Email { get; set; }
+        [PersonalData]
+        public string Password { get; set; }
     }
 }
