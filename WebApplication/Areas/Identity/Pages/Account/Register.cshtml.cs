2af007f7f242a2f2cfe655d802050231166d80cd
JJdoubleA <jaytdot2k@gmail.com>
2021-04-09 22:27:10 -0400

    check

--- WebApplication/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ WebApplication/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -76,8 +76,7 @@ namespace WebApplication.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
-            if (ModelState.IsValid)
-            {
+           
                     using var client = new HttpClient();
                 
                     client.BaseAddress = new Uri("https://rocket-rest-api.herokuapp.com/api/Customers/");
@@ -95,58 +94,34 @@ namespace WebApplication.Areas.Identity.Pages.Account
 
                     if (customer == true)
                     {
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
-                        protocol: Request.Scheme);
+                        if (ModelState.IsValid)
+                        {
+                            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                            var callbackUrl = Url.Page(
+                                "/Account/ConfirmEmail",
+                                pageHandler: null,
+                                values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
+                                protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-                     }
-                    if (customer == false)
-                    {
-                    //change line here *************\\\\\\\\\\\\------------------------------
-                    
-                    RedirectResult RedirectResult = Redirect("https://localhost:44317/Identity/Account/Register");
-                    return RedirectResult;
-                    }
-                    if (result.Succeeded)
-                    {
-                        _logger.LogInformation("User created a new account with password.");
-
-                        var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                        code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                        var callbackUrl = Url.Page(
-                            "/Account/ConfirmEmail",
-                            pageHandler: null,
-                            values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
-                            protocol: Request.Scheme);
-
-                        await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                            await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                             $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                             }
+                  
 
-                        if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                        {
-                            return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-                        }
-
-                        else
-                        {
-                            await _signInManager.SignInAsync(user, isPersistent: false);
-                            return LocalRedirect(returnUrl);
-                        }
+                    
+                            foreach (var error in result.Errors)
+                            {
+                                ModelState.AddModelError(string.Empty, error.Description);
+                            }
                     }
-                    foreach (var error in result.Errors)
-                    {
-                        ModelState.AddModelError(string.Empty, error.Description);
-                    }
-                }
-
-                // If we got this far, something failed, redisplay form
-                return Page();
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "You are not a customer yet");
+                    return Page();
+            }
+            // If we got this far, something failed, redisplay form
+            return null;
             }
         }
     }
